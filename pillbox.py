c3112051255845c33b68b0585866cf5932ae49e2
BasilVik <vikhlyaev.vasiliy@yandex.ru>
2024-02-29 20:58:58 +0700

    fix flake8

--- pillbox.py
+++ pillbox.py
@@ -1,19 +1,31 @@
-from config import TELEGRAM_TOKEN
-from log_config import logger
-
 from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters)
+
+from config import TELEGRAM_TOKEN
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
+    message_text = (f'Привет, {user.name}! Я бот PillBox'
+                    f'Я помогу составить расписание приёма лекарств,'
+                    f'а также напомню о приёме в нужное время.')
+    await update.message.reply_text(message_text)
+
     await update.message.reply_html(
         rf"Hi {user.mention_html()}!",
         reply_markup=ForceReply(selective=True),
     )
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def help_command(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Help!")
 
@@ -29,7 +41,9 @@ def main() -> None:
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND,
+                       echo))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
